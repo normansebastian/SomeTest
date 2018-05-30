b804d7c34d242a0c05f031173e58b7768254fd3a
normansebastian <38253015+normansebastian@users.noreply.github.com>
2018-05-30 10:16:50 +0200

    Added "AnotherClass"-class

---
+++ SomeTest/AnotherClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomeTest
+{
+    class AnotherClass
+    {
+        public int AnotherInt { get; set; }
+
+        public string AnotherString { get; set; }
+
+        public AnotherClass()
+        {
+            
+        }
+    }
+}
